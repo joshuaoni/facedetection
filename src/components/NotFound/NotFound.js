55bac4665b65028d37111094a6c23097fef73e3b
joshua <joshuadavidoni@yahoo.com>
2022-03-22 14:05:08 +0100

    new

--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -7,7 +7,7 @@ const NotFound = () => {
     return (
         <div className='not-found'>
             <h1 className='f2'>Oooooooops...</h1>
-            <h1>error 404 :(</h1>
+            <h1>error 404 :</h1>
             <h3 className='f3'>Looks like the link you clicked did not match any of our documents</h3>
             <Link className='f3 fw7' to='/'>Go back to login</Link>
         </div>
